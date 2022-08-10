027900cb2db6a19f85ab8786c60b8225be40b889
invisible360 <zakirh817@gmail.com>
2022-08-10 06:43:55 +0600

    first commit

---
+++ all-in-one.js
@@ -0,0 +1,11 @@
+/* 1) Harry’s mom gave him tk 1000 and asked him to buy some oranges and
+apples. Write a program to help Harry calculate how much money the
+shopkeeper will return. The total cost of 1 kg of oranges and 1 kg of
+apples is tk 700 */
+
+const moneyGiven = 1000;
+const costOfOrangeAndApples = 700;
+const shopkeeperReturn = moneyGiven - costOfOrangeAndApples;
+
+console.log (shopkeeperReturn);
+
